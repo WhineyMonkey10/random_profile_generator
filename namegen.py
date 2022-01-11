0750f1fe5b17687f93482f6e082f6071f471b910
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-01-11 18:19:45 +0100

    Critical Update
    Added a critical update with the return function not in place and optimizing & formatting the code.

--- namegen.py
+++ namegen.py
@@ -1,30 +1,31 @@
-import random
+def nameGenerator():
 
-middle_name = input("Do you want your name to include a middle name? (Yes or No?, Case sensitive). ")
+  import random
 
-if middle_name == ("Yes"):
   first_names = ["Tom", "Maya", "Jean", "Samerition", "Singual", "Bob", "Samantha", "Bart"]
-  random_number = random.randint(0, len(first_names) - 1 )
-  random_first_name = (''.join(first_names[random_number]))  
-
   middle_names = ["John", "Maxwell", "Kane", "Charles", "Louis", "Marley", "Drew", "Sam"]
-  random_number_2 = random.randint(0, len(first_names) - 1 )
-  random_middle_name = (''.join(middle_names[random_number_2]))
+  last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez"]
 
-  last_names = last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez"]
-  random_number_1 = random.randint(0, len(last_names) - 1 )
-  random_last_name = (''.join(last_names[random_number_1]))
+  middle_name_q = input("Do you want your name to include a middle name? (Yes or No?, Case sensitive). ")
 
-  final_name = (random_first_name + " " + random_middle_name + " "+ random_last_name)
-  print(final_name)
-
-else:
-  first_names = ["Tom", "Maya", "Jean", "Samerition", "Singual", "Bob", "Samantha", "Bart"]
+# Randomly Pick First Name
   random_number = random.randint(0, len(first_names) - 1 )
   random_first_name = (''.join(first_names[random_number]))  
-  last_names = last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez"]
+
+# Randomly pick a last name
   random_number_1 = random.randint(0, len(last_names) - 1 )
   random_last_name = (''.join(last_names[random_number_1]))
+  
+  if middle_name_q == ("Yes"): 
+  # Randomly Pick Middle Name
+    random_number_2 = random.randint(0, len(middle_names) -1)
 
-  final_name = (random_first_name + " " + random_last_name)
-  print(final_name)
+    random_middle_name = (''.join(middle_names[random_number_2]))
+  
+  # Name addition
+    final_name = (random_first_name + " " +        random_middle_name + " "+ random_last_name)
+    
+  else:
+   final_name = (random_first_name + " " + random_last_name)
+  
+  return final_name
