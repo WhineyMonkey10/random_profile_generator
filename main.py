943e0d782c4019ea6696ce727b9f1c1ec150c78a
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-01-11 17:06:23 +0100

    Added middle names.
    Added a middle name option when creating a random name for more option, names have also been updated.

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import random
 
+middle_name = input("Do you want your name to include a middle name? (Yes or No?, Case sensitive). ")
 
+if middle_name == ("Yes"):
+  first_names = ["Tom", "Maya", "Jean", "Samerition", "Singual", "Bob", "Samantha", "Bart"]
+  random_number = random.randint(0, len(first_names) - 1 )
+  random_first_name = (''.join(first_names[random_number]))  
 
-first_names = ["Jakob", "Oded", "Fraser", "Gabriel", "Adrien", "Bob", "Martin", "Bart"]
-random_number = random.randint(0, len(first_names) - 1 )
-random_first_name = (''.join(first_names[random_number]))  
+  middle_names = ["John", "Maxwell", "Kane", "Charles", "Louis", "Marley", "Drew", "Sam"]
+  random_number_2 = random.randint(0, len(first_names) - 1 )
+  random_middle_name = (''.join(middle_names[random_number_2]))
 
+  last_names = last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez"]
+  random_number_1 = random.randint(0, len(last_names) - 1 )
+  random_last_name = (''.join(last_names[random_number_1]))
 
-last_names = ["Mandel", "Dominic", "Samertons", "Simpson", "Nerd", "McDonald", "Pyjamas", "Pasta"]
-random_number_1 = random.randint(0, len(last_names) - 1 )
-random_last_name = (''.join(last_names[random_number_1]))
+  final_name = (random_first_name + " " + random_middle_name + " "+ random_last_name)
+  print(final_name)
 
-final_name = (random_first_name + " " + random_last_name)
-print(final_name)
+else:
+  first_names = ["Tom", "Maya", "Jean", "Samerition", "Singual", "Bob", "Samantha", "Bart"]
+  random_number = random.randint(0, len(first_names) - 1 )
+  random_first_name = (''.join(first_names[random_number]))  
+  last_names = last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez"]
+  random_number_1 = random.randint(0, len(last_names) - 1 )
+  random_last_name = (''.join(last_names[random_number_1]))
+
+  final_name = (random_first_name + " " + random_last_name)
+  print(final_name)
