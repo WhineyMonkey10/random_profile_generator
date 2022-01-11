d7d0cc95f1bd51e0a5cd86015a0be90558116f93
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-01-11 18:18:36 +0100

    Updated along with critical updates of namegen.py
    Updated this code along with namegen.py's critical update.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 ############### - Imports - ###############
 
+import namegen as ng
+
 from namegen import *
 
 
 ############### - Def Calls - ###############
+name = nameGenerator()
+print(name)
 
-nameGenerator()
+
 
