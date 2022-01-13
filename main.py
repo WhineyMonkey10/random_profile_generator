2498ba0ec2d5f17a26af223bab5598fff87a076c
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-01-13 19:31:40 +0100

    Updated for compatibility with addressgen.py & Added a disclaimer
    Update compatibility for addressgen.py and added a disclaimer before receiving the randomly generated profile.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from agegen import *
 
 from namegen import *
 
+from addressgen import *
+
+import time
+
 
 ############### - Def Calls - ###############
 
@@ -13,9 +17,17 @@ name = nameGenerator()
 # Defining the persons age
 age = ageGenerator()
 
-# Priting the above in a sentence
-print("Your name is:", name,", you are:", age, "years old!")
+# Defining the persons adddress
+addr = addrGenerator()
 
+# Priting the above in a sentence & Adding a disclaimer
+print("DISCLAIMER: None of these credentials, addresses, names, ages or others are meant to replicate a person's actual personal credentials.")
+
+# Adding time for the user to read the disclaimer
+time.sleep(1)
+
+# Actually printing the final product
+print("Your name is:", name, ", you live in", addr, ", and you are:", age, "years old!")
 
 
 
