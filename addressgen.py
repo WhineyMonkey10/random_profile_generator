6855c615d8353f2b56b43866281616f6260dc5e7
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-01-13 19:32:29 +0100

    Created addressgen.py

---
+++ addressgen.py
@@ -0,0 +1,25 @@
+def addrGenerator():
+
+  # Import packages
+  import random
+
+  # Define lists
+  countrycode = [",U.S.A", ",E.U", ",C.A"]
+  streetnames = ["Main Street", "Church Street", "Main Street North", "Main Street South", "Elm Street", "High Street", "Main Street West", "Washington Street", "Forster Close", "Alfred Street", "Allington Street", "Alma Road", "Somertrees Avenue", "Batson Street", "Bentinck Street", "George Street"]
+
+  # Assign each list their randomly generated number
+  housenum = [str(random.randint(1,2)), str(random.randint(1, 9))]
+
+  streetnum = random.randint(1, len(streetnames) -1)
+
+  countrycodenum = random.randint(1, len(countrycode) -1)
+
+  # Take the randomly generated number(s), then assign them to variables attached to the lists
+  streetnames = (streetnames[streetnum])
+  countrycode = (countrycode[countrycodenum])
+
+  # Join all/the of the variables and list together
+  inthousenum = (''.join(housenum))
+  finaladdr = (inthousenum + " " + streetnames + " " + countrycode)
+
+  return finaladdr
