4ada599f48c3bb6b20ab4f52a17c383fd914d8ba
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-01-25 11:30:51 +0100

    Fixed that it would not generate a middle name and added any type or yes combination
    Before this update, the program would not generator a middle name if told. The response to the question on line 9 of namegen.py has also been changed so your answer does no longer need to be case sensitive.

--- namegen.py
+++ namegen.py
@@ -6,7 +6,7 @@ def nameGenerator():
   middle_names = ["John", "Maxwell", "Kane", "Charles", "Louis", "Marley", "Drew", "Sam"]
   last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez"]
 
-  middle_name_q = input("Do you want your name to include a middle name? (Yes or No?, Case sensitive). ")
+  middle_name_q = input("Do you want your name to include a middle name? (Yes or No?). ")
 
 # Randomly Pick First Name
   random_number = random.randint(0, len(first_names) - 1 )
@@ -16,7 +16,7 @@ def nameGenerator():
   random_number_1 = random.randint(0, len(last_names) - 1 )
   random_last_name = (''.join(last_names[random_number_1]))
   
-  if middle_name_q == ("Yes"): 
+  if middle_name_q == ("Yes") or ("yEs") or ("yeS") or ("YES") or ("yea") or ("YEs") or ("yES") or ("YeS"): 
   # Randomly Pick Middle Name
     random_number_2 = random.randint(0, len(middle_names) -1)
 
