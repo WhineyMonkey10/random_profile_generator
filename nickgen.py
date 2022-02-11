c041bb1649d62a765bed50e8160dc1ea9ac1f050
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-02-11 14:52:04 +0100

    Created nickgen.py
    Added nickgen.py, it has not yet been implemented to main.py.

---
+++ nickgen.py
@@ -0,0 +1,7 @@
+def NickGen():
+  import random
+  ########## MAKING A LIST OF NICKNAMES #################
+  nicknames = ["`Kicker`", "`Gab`", "`FrayFray`", "`Burpy`", "`Nerdy`", "`Quintessential`", "`Kicker`", "`Gabitos`", "`Chrunou`", "`Michal`", "`Quickbot`", "`Quickbot`", "Bud"]
+  randomnicknum = (random.randint(1, len(nicknames)))
+  print (randomnicknum)
+  nickname = (nicknames[randomnicknum])
