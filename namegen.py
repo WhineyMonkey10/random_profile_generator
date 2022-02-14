61384159e53eac3828b6acfb2680bf669f8c8e51
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-02-14 12:21:29 +0100

    Added casefold
    Set middle_name_q to casefold so writing out all of the options is not necessary, also fixing it so case SenSiTivIty is no longer a problem, fixing the no matter what you said you would get a middle name.

--- namegen.py
+++ namegen.py
@@ -7,6 +7,7 @@ def nameGenerator():
   last_names = ["Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez", "Lee", "Never-Dies"]
 
   middle_name_q = input("Do you want your name to include a middle name? (Yes or No?). ")
+  middle_name_q = (middle_name_q.casefold())
 
 # Randomly Pick First Name
   random_number = random.randint(0, len(first_names) - 1 )
@@ -16,7 +17,7 @@ def nameGenerator():
   random_number_1 = random.randint(0, len(last_names) - 1 )
   random_last_name = (''.join(last_names[random_number_1]))
   
-  if middle_name_q == ("Yes") or ("yEs") or ("yeS") or ("YES") or ("yes") or ("YEs") or ("yES") or ("YeS"): 
+  if middle_name_q == ("yes"): 
   # Randomly Pick Middle Name
     random_number_2 = random.randint(0, len(middle_names) -1)
 
@@ -25,9 +26,6 @@ def nameGenerator():
   # Name addition
     final_name = (random_first_name + " " +        random_middle_name + " "+ random_last_name)
     
-  if middle_name_q == ("no") or ("No") or ("NO") or ("nO"): 
+  if middle_name_q == ("no"): 
    final_name = (random_first_name + " " + random_last_name)
   return final_name
-
-
-  
