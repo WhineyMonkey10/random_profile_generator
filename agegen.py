f4a53362b05c7753c5f40b4b04cb523aec8ec8ee
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-04-02 22:59:00 +0200

    Fixed the high chances of being dead or unborn
    Being dead or unborn used to have high chances, now there is a 2/10 chance you are either. (20%)

--- agegen.py
+++ agegen.py
@@ -2,14 +2,15 @@ def ageGenerator():
   import random
   
   # Creating the random age
-  probagenum = random.randint(1, 3)
+  probagenum = random.randint(1, 10)
   
-  if probagenum == 1:
+  if probagenum == 1 or 3 or 4 or 6 or 7 or 8 or 9 or 10:
     age = random.randint(1, 99)
   else:
     if probagenum == 2:
       age = ("Unborn (Ignore years old)")
      else:
-       age = ("Dead (Ignore years old)")
+      if probagenum == 5:
+        age = ("Dead (Ignore years old)")
   
   return age
