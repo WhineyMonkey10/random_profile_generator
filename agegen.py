a4120d70d18400c0ac9bed11c7c843fc00a064cc
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-04-02 22:55:30 +0200

    added the possibility to be unborn

--- agegen.py
+++ agegen.py
@@ -2,5 +2,11 @@ def ageGenerator():
   import random
   
   # Creating the random age
-  age = random.randint(1, 99)
+  probagenum = random.randint(1, 2)
+  
+  if probagenum == 1:
+    age = random.randint(1, 99)
+  else:
+    age = ("Unborn")
+  
   return age
