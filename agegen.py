391b333765a7d6b96bf2f82a4dec3849aca6d9b8
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-04-02 22:57:15 +0200

    Option to be dead
    Option to be dead, and also said ignore the years old in the final result

--- agegen.py
+++ agegen.py
@@ -2,11 +2,14 @@ def ageGenerator():
   import random
   
   # Creating the random age
-  probagenum = random.randint(1, 2)
+  probagenum = random.randint(1, 3)
   
   if probagenum == 1:
     age = random.randint(1, 99)
   else:
-    age = ("Unborn")
+    if probagenum == 2:
+      age = ("Unborn (Ignore years old)")
+     else:
+       age = ("Dead (Ignore years old)")
   
   return age
