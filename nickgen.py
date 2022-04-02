14ea2d27e9939c89063138b7c9f96130ac2e45cd
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-04-02 23:00:26 +0200

    Removed a useless print

--- nickgen.py
+++ nickgen.py
@@ -3,7 +3,6 @@ def nickGen():
   ########## MAKING A LIST OF NICKNAMES #################
   nicknames = ["Kicker", "Gab", "FrayFray", "Burpy", "Nerdy", "Quintessential", "Kicker", "Gabitos", "Chrunou", "Michal", "Mr.Fries", "Quickbot", "Bud", "HogeFoot"]
   randomnicknum = (random.randint(1, len(nicknames)))
-  print (randomnicknum)
   nickname = (nicknames[randomnicknum])
   return nickname
   
