74d7640ea835c6b751dfd5e3fd255b85229d5c3b
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-04-08 14:22:06 +0200

    removed an uncessasacry print, removed repetition
    There was a print function that called the random number for the nickname. There was also a nickname repitition.

--- nickgen.py
+++ nickgen.py
@@ -1,7 +1,7 @@
 def nickGen():
   import random
   ########## MAKING A LIST OF NICKNAMES #################
-  nicknames = ["Kicker", "Gab", "FrayFray", "Burpy", "Nerdy", "Quintessential", "Kicker", "Gabitos", "Chrunou", "Michal", "Mr.Fries", "Quickbot", "Bud", "HogeFoot"]
+  nicknames = ["Kicker", "Gab", "FrayFray", "Burpy", "Nerdy", "Quintessential", "Kicker", "Gabitos", "Chrunou", "Michal", "Quickbot", "Hogefoot", "Bud"]
   randomnicknum = (random.randint(1, len(nicknames)))
   nickname = (nicknames[randomnicknum])
   return nickname
