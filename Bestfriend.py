1b60b3dda32b1219bf35bbac41cb4f13fb22123a
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-05-02 09:20:47 +0200

    Initial commit

---
+++ Bestfriend.py
@@ -0,0 +1,13 @@
+import random
+
+def your_best_friend():
+
+  list_of_names = ["Tom", "Maya", "Jean", "Samerition", "Singual", "Bob", "Samantha", "Bart", "Stan", "Cobra", "John", "Maxwell", "Kane", "Charles", "Louis", "Marley", "Drew", "Sam", "Stanley", "Kai", "Holland", "Dominic", "Samertons", "Simpson", "Zinkovic", "McDonald", "Sabet", "Vertez", "Lee",]
+
+  random_friendnum = random.randint (0,29)
+
+  list_of_names1 = ((list_of_names[random_friendnum]))
+
+  list_of_names = (f"{list_of_names1}")
+
+  return list_of_names
