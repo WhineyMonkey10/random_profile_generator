37956d61803b5476d4bd26efbf32cbe9b1aac521
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-05-07 11:39:18 +0200

    Fixed local server

--- src/localserver/localserver.py
+++ src/localserver/localserver.py
@@ -1,45 +1,17 @@
-fileloc = input("Please enter the file location, for example : C:\\Users\\WhMonkey\\Downloads\\random_profile_generator\\src\\localserver : ")
-
-import os
+# Import libraries
 import http.server
 import socketserver
-from os import path
-user = os.path.abspath(os.getcwd())
-my_host_name = 'localhost'
-my_port = 8888
-my_html_folder_path = f'{fileloc}'
-
-my_home_page_file_path = 'index.html'
-
-
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', path.getsize(self.getPath()))
-        self.end_headers()
-
-    def getPath(self):
-        if self.path == '/':
-            content_path = path.join(
-                my_html_folder_path, my_home_page_file_path)
-        else:
-            content_path = path.join(my_html_folder_path, str(self.path).split('?')[0][1:])
-        return content_path
-
-    def getContent(self, content_path):
-        with open(content_path, mode='r', encoding='utf-8') as f:
-            content = f.read()
-        return bytes(content, 'utf-8')
-
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(self.getContent(self.getPath()))
-
-
-my_handler = MyHttpRequestHandler
-
-with socketserver.TCPServer(("", my_port), my_handler) as httpd:
-    print("Http Server Serving at port", my_port)
-    httpd.serve_forever()
+import colorama
+  
+# Defining PORT number
+PORT = 8080
+  
+# Creating handle
+Handler = http.server.SimpleHTTPRequestHandler
+  
+# Creating TCPServer
+http = socketserver.TCPServer(("", PORT), Handler)
+  
+# Getting logs
+print(f"{colorama.Fore.CYAN}LocaL server {colorama.Fore.GREEN}OPENED{colorama.Fore.CYAN} and bound to the port: {PORT}")
+http.serve_forever()
---
+++ src/localserver/main.py
@@ -0,0 +1,83 @@
+############### - Imports - ###############
+
+from numpy import true_divide
+from src.gens.agegen import *
+
+from src.gens.namegen import *
+
+from src.gens.addressgen import *
+
+from src.gens.nickgen import *
+
+from src.gens.allergies import *
+
+from src.gens.favouritefood import *
+
+from src.gens.borngen import *
+
+from src.gens.Bestfriend import *
+
+from src.gens.eyecolourgen import *
+
+from src.gui.main import *
+
+import time
+
+from tkinter import *
+
+import PySimpleGUI as sg
+
+import colorama
+
+from colorama import Back, Fore, Style
+
+############### - Def Calls - ###############
+
+# Defining the profiles name
+name = nameGenerator()
+
+# Defining the profiles age
+age = ageGenerator()
+
+# Defining the profiles adddress
+addr = addrGenerator()
+
+#Defining the profiles nickname
+nick = nickGen()
+
+#Defining the profiles allergy
+allergie = allergiesGen()
+
+#Defining the profiles favourite food
+favfood = favouritefood()
+
+#Defining the profiles location of birth
+born = borngen()
+
+#Definfing the profiles best friend
+bestfriend = your_best_friend()
+
+#Defining the profiles eye colour
+eyecolour = eyecolourgen()
+
+#Auto Reset
+colorama.init(autoreset=True)
+
+# Priting the above in a sentence & Adding a disclaimer
+print("DISCLAIMER: ")
+time.sleep(0.5)
+print(Fore.RED + "None of these credentials, addresses, names, ages or others are meant to replicate a person's actual personal credentials. Any malicious use of this project is not my responsability, this is for educational purposes only.")
+
+# Adding time for the user to read the disclaimer
+time.sleep(1)
+
+type = input(f"{Fore.CYAN}Please enter the type of profile you would like to generate (web server, gui, text): ")
+type = (type.casefold())
+
+#Initialising the final product
+finalres = f"{Fore.CYAN}Your name is: {name} also known as (your nickname) {nick},  you live in {addr}, and you are: {age} years old! {allergie}. {favfood}. You were born in: {born} and your BFF is {bestfriend}. Your eye colour is: {eyecolour}"
+
+
+#input("Do you want to save this profile? (Yes or No)")
+
+
