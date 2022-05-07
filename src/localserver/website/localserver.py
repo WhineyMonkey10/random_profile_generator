2fc896d169a33fa328e4609a1cc9dbca1d318bce
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-05-07 11:45:41 +0200

    Finished localserver creation saupport

---
+++ src/localserver/website/localserver.py
@@ -0,0 +1,17 @@
+# Import libraries
+import http.server
+import socketserver
+import colorama
+  
+# Defining PORT number
+PORT = 8080
+  
+# Creating handle
+Handler = http.server.SimpleHTTPRequestHandler
+  
+# Creating TCPServer
+http = socketserver.TCPServer(("", PORT), Handler)
+  
+
+print(f"{colorama.Fore.CYAN}LocaL server {colorama.Fore.GREEN}OPENED{colorama.Fore.CYAN} and bound to the port: {PORT}. Copy paste this link into your browser: http://localhost::{PORT}/src/localserver/website/{colorama.Fore.RESET}")
+http.serve_forever()
