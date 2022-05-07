88e816dc0b623cb593bf495593e931cba178d768
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-05-07 11:58:34 +0200

    Nothing special.

--- src/localserver/scripts.js
+++
@@ -1,3 +0,0 @@
-function display() {
-    alert("Hello World!");
-  }
---
+++ src/localserver/website/scripts.js
@@ -0,0 +1,3 @@
+function alert() {
+    alert("Hello, the Web version of Random Profile Generator is running, but not complete. Please wait for the full version to be released.");
+  }
