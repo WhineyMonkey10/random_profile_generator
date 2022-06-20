4feb23ab19968569c9718949623c1c133da5afed
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-06-20 17:27:39 +0200

    Slowly and painfully fixing flask errors

--- src/gens/favouritefood.py
+++ src/gens/favouritefood.py
@@ -9,7 +9,7 @@ def favouritefood():
 
   favfood1 = (''.join((favfood[random_foodnum])))
 
-  favfood = (f"Your favourite food is: {favfood1}")
+  favfood = (f"Your favourite food are: {favfood1}")
 
 
   return favfood
