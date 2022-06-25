db345c9a0fe5423c499f0458f44b760a22691a65
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-06-25 16:59:40 +0200

    Inserting variable into generation page

--- main.py
+++ main.py
@@ -104,26 +104,29 @@ elif type == "web server":
 
     from flask import Flask, render_template, Response, request, redirect, url_for
     from src import *
-#from src import *
-app = Flask(__name__, template_folder='src/localserver/website/pages', static_folder='src/localserver/website/static')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/generate', methods=['POST'])
-def generate():
-    return render_template('generation.html')
-
-
-@app.route("/generate", methods=['POST', 'GET'])
-def move_forward():
-    if request.method == 'POST' or 'GET':
-        return redirect(url_for('generate'))
-
-app.run()
+    #from src import *
+    app = Flask(__name__, template_folder='src/localserver/website/pages', static_folder='src/localserver/website/static')
     
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    @app.route('/generate', methods=['POST'])
+    def generate():
+        return render_template('generation.html')
+    
+    @app.route("/generate/<username>")
+    def greet(username):
+        return "Hello " + username + "!"
+    
+    
+    @app.route("/generate", methods=['POST', 'GET'])
+    def move_forward():
+        if request.method == 'POST' or 'GET':
+            return redirect(url_for('generate'))
+    
+    app.run()
+
     
 
 
